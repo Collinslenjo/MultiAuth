83e16f7afe2d5055d2a2d429bdc64c419f89827d
Collins Lenjo <collinslenjo.cl@gmail.com>
2017-04-24 13:28:18 +0300

    part 2 done(Auth from admin and user)

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,15 +4,32 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Auth;
 
 class AdminLoginController extends Controller
 {
+    public function __construct() {
+      $this->middleware('guest:admin');
+    }
     public function showLoginForm() {
       return view('auth.admin-login');
     }
 
-    public function login()
+    public function login(Request $request)
     {
-      return true;
+      // validate form data
+      $this->validate($request, [
+        'email' => 'required|email',
+        'password' => 'required|min:6'
+      ]);
+
+      // attemt to login user
+      if(Auth::guard('admin')->attempt(['email'=>$request->email, 'password'=>$request->password],$request->remember))
+      {
+      // if successful redirect to the intended location
+      return redirect()->intended(route('admin.dashboard'));
+      }
+      // if unsuccessful redirect back to admin/login with the login form data
+      return redirect()->back()->withInput($request->only('email','remember'));
     }
 }
